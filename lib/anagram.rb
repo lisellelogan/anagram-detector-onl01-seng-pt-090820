1cb96a343ec6090a8afb5fb220e13a4954393a21
Liselle Logan <liselle.logan@gmail.com>
2020-10-02 00:40:01 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = []
+  end
+
+  def match(word)
+    @word.select{|matching_word| matching_word.name == word}
+  end
+
+end
